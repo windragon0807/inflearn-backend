57d1d84957f3022231dfa902e9bca291ecb01096
windragon0807 <tmdfyd1995@gmail.com>
2024-06-25 18:56:15 +0900

    express, nodemon, swagger, cors

---
+++ section5/backend/phone.js
@@ -0,0 +1,20 @@
+// phone.js
+
+export function checkPhone(myphone) {
+  if (myphone.length < 10 || myphone.length > 11) {
+    console.log("에러 발생!!! 핸드폰 번호를 제대로 입력해 주세요!!!");
+    return false;
+  } else {
+    return true;
+  }
+}
+
+export function getToken() {
+  const result = String(Math.floor(Math.random() * 1000000)).padStart(6, "0");
+  console.log(result);
+  return result;
+}
+
+export function sendTokenToSMS(myphone, result) {
+  console.log(myphone + "번호로 인증번호 " + result + "를 전송합니다.");
+}
